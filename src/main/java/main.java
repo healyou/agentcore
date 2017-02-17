6968d0cd27e98cf9d836eb0e55e0f587c3fc8f4d
Nikita Gorodilov <healyou1994@gmail.com>
2017-02-17 22:32:10 +0300

    добавлен main для мини тестиков кода

---
+++ src/main/java/main.java
@@ -0,0 +1,24 @@
+import inputdata.inputdataverification.InputDataVerificationImpl;
+import inputdata.inputdataverification.inputdata.InputTableDesc;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * Created on 17.02.2017 12:03
+ *
+ * @autor Nikita Gorodilov
+ */
+public class main {
+
+    public static void main(String[] args) {
+        InputDataVerificationImpl dataVerification = new InputDataVerificationImpl();
+        try {
+            InputTableDesc tableDesc = dataVerification.
+                    getDatabaseTables("C:\\Users\\lappi\\Intellij IDEA\\Projects\\agentcore\\src\\test\\resources\\inputdata\\inputdataverification\\tableDescription.xml");
+            JdbcTemplate jdbcTemplate = dataVerification.getJdbcTemplate("src\\test\\resources\\inputdata\\inputdataverification\\testdb.properties");
+            dataVerification.testReadDbData(jdbcTemplate, tableDesc);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+}
