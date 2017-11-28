be3d3e581b7a664d31be34eb967bab95d8cd4eee
Nikita Gorodilov <healyou1994@gmail.com>
2017-11-28 18:23:24 +0300

    типы тестовых агентов

---
+++ agentcore/src/test/java/dsl/IntegrationRuntimeAgentTest.groovy
@@ -0,0 +1,19 @@
+package dsl
+
+import org.junit.Ignore
+import org.junit.Test
+import testbase.AbstractServiceTest
+
+/**
+ * @author Nikita Gorodilov
+ */
+@Ignore
+class IntegrationRuntimeAgentTest extends AbstractServiceTest {
+
+    // TODO переименовать все groovy тестовые методы таким образом, как снизу
+
+    @Test
+    void "тестовый метод"() {
+
+    }
+}
